8360e2914cffe96214868dc9fc0cb718a7dd532e
Oleg Shevchuk <shef2504@gmail.com>
2015-05-17 17:53:32 +0300

    работает

--- Loader.java
+++ Loader.java
@@ -1,8 +1,6 @@
 package ua.artcode.ds.Week4.Loader;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -15,7 +13,7 @@ public class Loader
 {
     private  String url;
     private String fileName;
-    private String folderSafe="/temp";
+    private String folderSafe="D:\\RUSH\\Project\\JavaRushHomeWork\\temp";
     public Loader(String url){
         this.url=url;
         readPage();
@@ -66,8 +64,10 @@ public class Loader
                 String line=br.readLine();
                 if (line.contains("mp3")&&line.contains("title")&&line.contains("get")) {
                     String fileUrl="http://www.ex.ua/"+line.substring(line.indexOf("get/"), line.indexOf("' title"));
-                    System.out.println(fileUrl);
-                    load(fileUrl);
+                    String fileName="/"+line.substring(line.indexOf("title='"),line.indexOf("mp3")).substring(11)+"mp3";
+                    System.out.print(fileUrl);
+                    System.out.println(fileName);
+                    load(fileUrl,fileName);
                 }
                 //if (line.contains("mp3")) System.out.println(line);
             }
@@ -85,7 +85,38 @@ public class Loader
             e.printStackTrace();
         }
     }
-    private void load(String url){
-        
+    private void load(String url, String fileName){
+        url=url.replace("get","load");
+        System.out.println(url);
+        URL url1 = null;
+        try
+        {
+            url1 = new URL(url);
+        }
+        catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        }
+        try
+                (
+
+
+            FileOutputStream writer=new FileOutputStream(folderSafe+fileName)){
+            InputStream is=url1.openStream();
+            byte[] buff = new byte[1000000];
+            int count = 0;
+            while((count = is.read(buff)) != -1){
+                writer.write(buff,0,count);
+                writer.flush();
+            }
+        }
+        catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
